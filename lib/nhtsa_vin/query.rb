50ae06a93e6da44aed5c669dd92be3fec94a2f3c
Aldo Delgado <aldo@osstek.com>
2025-08-05 02:54:57 -0400

    Update query.rb

--- lib/nhtsa_vin/query.rb
+++ lib/nhtsa_vin/query.rb
@@ -8,7 +8,7 @@ module NhtsaVin
 
     attr_reader :vin, :url, :response, :data, :error, :error_code, :raw_response
 
-    def initialize(vin, options={})
+    def initialize(vin, options = {})
       @vin = vin.strip.upcase
       @http_options = options[:http] || {}
       build_url
@@ -37,104 +37,141 @@ module NhtsaVin
         @error = json.dig('Results', 0, 'Message')
         return
       end
+
       @data = json['Results']
-
-      # 0 - Good
-      # 1 - VIN decoded clean. Check Digit (9th position) does not calculate properly.
-      # 3 - VIN corrected, error in one position (assuming Check Digit is correct).
-      # 5 - VIN has errors in few positions.
-      # 6 - Incomplete VIN.
-      # 8 - No detailed data available currently.
-      # 11- Incorrect Model Year, decoded data may not be accurate!
       @error_code = value_id_for('Error Code')&.to_i
-      @valid = (@error_code < 4) ? true : false
+      @valid = (@error_code < 4)
 
-      if @error_code != 0
-        @error = value_for('Error Code')
-      end
-      return nil unless valid?
+      @error = value_for('Error Code') unless @valid?
+      return unless valid?
 
-      make = value_for('Make')&.capitalize
-      model = value_for('Model')
-      trim = value_for('Trim')
-      year = value_for('Model Year')
-      style = value_for('Body Class')
-      type = vehicle_type(style, value_for('Vehicle Type'))
-      doors = value_for('Doors')&.to_i
-
-      @response = Struct::NhtsaResponse.new(@vin, make, model, trim, type, year,
-                                            style, value_for('Vehicle Type'), doors)
+      @response = Struct::NhtsaExtendedResponse.new(
+        @vin,
+        value_for('Make')&.capitalize,
+        value_for('Model'),
+        value_for('Trim'),
+        vehicle_type(value_for('Body Class'), value_for('Vehicle Type')),
+        value_for('Model Year'),
+        value_for('Body Class'),
+        value_for('Vehicle Type'),
+        value_for('Doors')&.to_i,
+        value_for('Manufacturer Name'),
+        value_for('Series'),
+        value_for('Trim2'),
+        value_for('Series2'),
+        value_for('Note'),
+        value_for('Gross Vehicle Weight Rating From'),
+        value_for('Bed Length (inches)'),
+        value_for('Curb Weight (pounds)'),
+        value_for('Wheel Base (inches) From'),
+        value_for('Wheel Base (inches) To'),
+        value_for('Gross Combination Weight Rating From'),
+        value_for('Gross Combination Weight Rating To'),
+        value_for('Gross Vehicle Weight Rating To'),
+        value_for('Bed Type'),
+        value_for('Cab Type'),
+        value_for('Wheel Size Front (inches)'),
+        value_for('Wheel Size Rear (inches)'),
+        value_for('Drive Type'),
+        value_for('Brake System Type'),
+        value_for('Engine Number of Cylinders'),
+        value_for('Fuel Type - Primary'),
+        value_for('Engine Configuration'),
+        value_for('Engine Brake (hp) From'),
+        value_for('Engine Manufacturer'),
+        value_for('Front Air Bag Locations'),
+        value_for('Side Air Bag Locations'),
+        value_for('Anti-lock Braking System (ABS)'),
+        value_for('Electronic Stability Control (ESC)'),
+        value_for('Traction Control'),
+        value_for('Tire Pressure Monitoring System (TPMS) Type'),
+        value_for('Auto-Reverse System for Windows and Sunroofs'),
+        value_for('Keyless Ignition'),
+        value_for('Adaptive Cruise Control (ACC)'),
+        value_for('Crash Imminent Braking (CIB)'),
+        value_for('Forward Collision Warning (FCW)'),
+        value_for('Dynamic Brake Support (DBS)'),
+        value_for('Blind Spot Warning (BSW)'),
+        value_for('Backup Camera'),
+        value_for('Rear Cross Traffic Alert'),
+        value_for('Rear Automatic Emergency Braking'),
+        value_for('Daytime Running Light (DRL)'),
+        value_for('Headlamp Light Source'),
+        value_for('Semiautomatic Headlamp Beam Switching')
+      )
     end
 
     def vehicle_type(body_class, type)
-      return case type
+      case type
       when 'PASSENGER CAR'
         'Car'
       when 'TRUCK'
-        if body_class =~ /van/i
-          'Van'
-        else
-          'Truck'
-        end
+        body_class =~ /van/i ? 'Van' : 'Truck'
       when 'MULTIPURPOSE PASSENGER VEHICLE (MPV)'
-        if body_class =~ /Sport Utility/i
-          'SUV'
-        else
-          'Minivan'
-        end
+        body_class =~ /Sport Utility/i ? 'SUV' : 'Minivan'
+      else
+        type
       end
     end
 
     private
 
-      def get_row(attr_name)
-        @data.select { |r| r['Variable'] == attr_name }.first
-      end
+    def get_row(attr_name)
+      @data.find { |r| r['Variable'] == attr_name }
+    end
 
-      def value_for(attr_name)
-        get_row(attr_name)['Value']
-      end
+    def value_for(attr_name)
+      get_row(attr_name)&.[]('Value')
+    end
 
-      def value_id_for(attr_name)
-        get_row(attr_name)['ValueId']
-      end
+    def value_id_for(attr_name)
+      get_row(attr_name)&.[]('ValueId')
+    end
 
-      def build_url
-        @url = "#{NHTSA_URL}#{@vin}?format=json"
-      end
+    def build_url
+      @url = "#{NHTSA_URL}#{@vin}?format=json"
+    end
 
-      def fetch
-        begin
-          @valid = false
-
-          url = URI.parse(@url)
-          http_options = { use_ssl: (url.scheme == 'https') }
-          http_options.update(@http_options)
-          Net::HTTP.start(url.host, url.port, http_options) do |http|
-            resp = http.request_get(url)
-            case resp
-            when Net::HTTPSuccess
-              @valid = true
-              resp.body
-            when Net::HTTPRedirection
-              raise 'No support for HTTP redirection from NHTSA API'
-            when Net::HTTPClientError
-              @error = "Client error: #{resp.code} #{resp.message}"
-              nil
-            else
-              @error = resp.message
-              nil
-            end
+    def fetch
+      begin
+        @valid = false
+        url = URI.parse(@url)
+        http_options = { use_ssl: (url.scheme == 'https') }.merge(@http_options)
+        Net::HTTP.start(url.host, url.port, http_options) do |http|
+          resp = http.request_get(url)
+          case resp
+          when Net::HTTPSuccess
+            @valid = true
+            resp.body
+          when Net::HTTPRedirection
+            raise 'No support for HTTP redirection from NHTSA API'
+          when Net::HTTPClientError
+            @error = "Client error: #{resp.code} #{resp.message}"
+            nil
+          else
+            @error = resp.message
+            nil
           end
-        rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, SocketError,
-               Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
-               Net::ProtocolError, Errno::ECONNREFUSED => e
-          @error = e.message
-          nil
         end
+      rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, SocketError,
+        Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
+        Net::ProtocolError, Errno::ECONNREFUSED => e
+        @error = e.message
+        nil
       end
+    end
   end
 
-  Struct.new('NhtsaResponse', :vin, :make, :model, :trim, :type, :year,
-             :body_style, :vehicle_class, :doors)
+  Struct.new(
+    'NhtsaExtendedResponse',
+    :vin, :make, :model, :trim, :type, :year, :body_style, :vehicle_class, :doors,
+    :manufacturer_name, :series, :trim2, :series2, :note, :gvwr_from, :bed_length,
+    :curb_weight, :wheelbase_from, :wheelbase_to, :gcwr_from, :gcwr_to, :gvwr_to,
+    :bed_type, :cab_type, :wheel_size_front, :wheel_size_rear, :drive_type,
+    :brake_system_type, :engine_cylinders, :fuel_type, :engine_config,
+    :engine_hp_from, :engine_manufacturer, :front_airbags, :side_airbags,
+    :abs, :esc, :traction_control, :tpms, :auto_reverse,
+    :keyless_ignition, :adaptive_cruise, :cib, :fcw, :dbs, :bsw,
+    :backup_camera, :rear_cross_traffic, :rear_aeb, :drl, :headlamp_source, :semi_auto_headlamp
+  )
 end
